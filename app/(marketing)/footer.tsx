4b5386adc9550204fd7f8b8b0d0fe0545df2f106
Diego de Souza Santana <diegossantana068@gmail.com>
2024-04-25 19:38:32 -0300

    marketing skeleton

---
+++ app/(marketing)/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <footer className="hidden lg:block h-20 w-full border-t-2 border-[#1f1f1f] p-2 text-white">
+            Footer
+        </footer>
+    )
+}
---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <header className="h-20 w-full border-b-2 border-[#1f1f1f] text-white px-4">
+            Header
+        </header>
+    )
+}
