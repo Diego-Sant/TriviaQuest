1f89253732ac056898fe1d24b015b1506bc57f22
Diego de Souza Santana <diegossantana068@gmail.com>
2024-05-03 03:08:42 -0300

    seed att pt.1

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -31,11 +31,11 @@ const main = async () => {
 
         // await db.insert(schema.units).values([
         //     {
-        //         id: 1,
+        //         id: 3,
         //         categoryId: 1,
-        //         title: "Naruto",
-        //         description: "Você realmente conhece Naruto?",
-        //         order: 1,
+        //         title: "Naruto (Médio)",
+        //         description: "Continue! Está indo pelo caminho certo!",
+        //         order: 3,
         //     },
         //     {
         //         id: 2,
@@ -48,99 +48,163 @@ const main = async () => {
 
         // await db.insert(schema.quizzes).values([
         //     {
-        //         id: 3,
-        //         unitId: 1,
+        //         id: 6,
+        //         unitId: 2,
+        //         order: 1,
+        //         title: "Nível 1(Iniciante)"
+        //     },
+        //     {
+        //         id: 7,
+        //         unitId: 2,
+        //         order: 2,
+        //         title: "Nível 2(Iniciante)"
+        //     },
+        //     {
+        //         id: 8,
+        //         unitId: 2,
         //         order: 3,
         //         title: "Nível 3(Iniciante)"
         //     },
         //     {
-        //         id: 4,
-        //         unitId: 1,
+        //         id: 9,
+        //         unitId: 2,
         //         order: 4,
-        //         title: "Nível 4(Iniciante)"
+        //         title: "Nível 4(Básico)"
         //     },
         //     {
-        //         id: 5,
-        //         unitId: 1,
+        //         id: 10,
+        //         unitId: 2,
         //         order: 5,
         //         title: "Nível 5(Básico)"
         //     },
         // ])
 
-        // await db.insert(schema.challenges).values([
-        //     {
-        //         id: 4,
-        //         quizId: 2,
-        //         type: "ASSIST",
-        //         order: 1,
-        //         question: 'Qual dos personagens fazia parte da Akatsuki?'
-        //     },
-        //     {
-        //         id: 5,
-        //         quizId: 2,
-        //         type: "SELECT",
-        //         order: 2,
-        //         question: 'Quem foi o segundo hokage?'
-        //     }
-        // ]);
+        await db.insert(schema.challenges).values([
+            {
+                id: 21,
+                quizId: 10,
+                type: "ASSIST",
+                order: 1,
+                question: 'Quem é o Deus da Destruição do universo 6?'
+            },
+            {
+                id: 22,
+                quizId: 10,
+                type: "ASSIST",
+                order: 2,
+                question: 'Qual personagem ganhou o Torneio do Poder?'
+            },
+        ]);
 
         // await db.insert(schema.challengeOptions).values([
         //     {
-        //         id: 17,
-        //         challengeId: 5,
-        //         imageSrc: "/quizzes/hiruzen.svg",
+        //         id: 73,
+        //         challengeId: 19,
+        //         imageSrc: "/quizzes/goku.svg",
         //         correct: false,
-        //         text: "Hiruzen Sarutobi"
+        //         text: "Goku"
         //     },
         //     {
-        //         id: 18,
-        //         challengeId: 5,
-        //         imageSrc: "/quizzes/hashirama.svg",
+        //         id: 74,
+        //         challengeId: 19,
+        //         imageSrc: "/quizzes/broly.svg",
         //         correct: false,
-        //         text: "Hashirama Senju"
+        //         text: "Broly"
         //     },
         //     {
-        //         id: 19,
-        //         challengeId: 5,
-        //         imageSrc: "/quizzes/minato.svg",
+        //         id: 75,
+        //         challengeId: 19,
+        //         imageSrc: "/quizzes/bardock.svg",
         //         correct: false,
-        //         text: "Minato Namikaze"
+        //         text: "Bardock"
         //     },
         //     {
-        //         id: 20,
-        //         challengeId: 5,
-        //         imageSrc: "/quizzes/tobirama.svg",
+        //         id: 76,
+        //         challengeId: 19,
+        //         imageSrc: "/quizzes/vegeta.svg",
+        //         correct: true,
+        //         text: "Vegeta"
+        //     },
+        //     {
+        //         id: 69,
+        //         challengeId: 18,
+        //         imageSrc: "/quizzes/satan.svg",
+        //         correct: true,
+        //         text: "Mr. Satan"
+        //     },
+        //     {
+        //         id: 70,
+        //         challengeId: 18,
+        //         imageSrc: "/quizzes/kaio.svg",
         //         correct: false,
-        //         text: "Tobirama Senju"
+        //         text: "Senhor Kaio"
+        //     },
+        //     {
+        //         id: 71,
+        //         challengeId: 18,
+        //         imageSrc: "/quizzes/whis.svg",
+        //         correct: false,
+        //         text: "Whis"
+        //     },
+        //     {
+        //         id: 72,
+        //         challengeId: 18,
+        //         imageSrc: "/quizzes/kame.svg",
+        //         correct: false,
+        //         text: "Mestre Kame"
         //     },
         // ]);
 
-        // await db.insert(schema.challengeOptions).values([
-        //     {
-        //         id: 13,
-        //         challengeId: 4,
-        //         correct: false,
-        //         text: "Gaara"
-        //     },
-        //     {
-        //         id: 14,
-        //         challengeId: 4,
-        //         correct: true,
-        //         text: "Kimimaro Kaguya"
-        //     },
-        //     {
-        //         id: 15,
-        //         challengeId: 4,
-        //         correct: true,
-        //         text: "Sasori"
-        //     },
-        //     {
-        //         id: 16,
-        //         challengeId: 4,
-        //         correct: false,
-        //         text: "Zabuza Momochi"
-        //     },
-        // ]);
+        await db.insert(schema.challengeOptions).values([
+            {
+                id: 81,
+                challengeId: 21,
+                correct: true,
+                text: "Champa"
+            },
+            {
+                id: 82,
+                challengeId: 21,
+                correct: false,
+                text: "Bills"
+            },
+            {
+                id: 83,
+                challengeId: 21,
+                correct: false,
+                text: "Sidra"
+            },
+            {
+                id: 84,
+                challengeId: 21,
+                correct: false,
+                text: "Liquiir"
+            },
+            {
+                id: 85,
+                challengeId: 22,
+                correct: false,
+                text: "Goku"
+            },
+            {
+                id: 86,
+                challengeId: 22,
+                correct: true,
+                text: "Android 17"
+            },
+            {
+                id: 87,
+                challengeId: 22,
+                correct: false,
+                text: "Freeza"
+            },
+            {
+                id: 88,
+                challengeId: 22,
+                correct: false,
+                text: "Jiren"
+            },
+        ]);
 
         console.log("Acesso finalizado!");
     } catch (error) {
